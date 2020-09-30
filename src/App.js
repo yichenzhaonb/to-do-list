e26cd72a16f26ed9855dfc0bbad23d37fd230a6b
Yichen <8408937+yichenzhaonb@users.noreply.github.com>
2020-09-30 17:14:05 -0500

    add new feature (#2)
    * add Form
    
    * create add task feature

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Form from "./components/Form";
-function App() {
+import Todo from "./components/Todo";
+import { nanoid } from "nanoid";
+
+
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+function addTask(name){
+  let newTask = {
+                id:"todo-" + nanoid(),
+                name: name,
+                complete:false
+                };
+  
+  setTasks([...tasks,newTask]);
+
+}
+
+const taskList = tasks.map(task => (
+  <Todo
+    id={task.id}
+    name={task.name}
+    completed={task.completed}
+    key={task.id}
+  />
+));
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,8 +35,15 @@ function App() {
         </p>
       </header>
       <div className="Content">
+        <Form addTask= {addTask} />
+        <ul  
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
         <Form />
-       {/* add content */}
+
       </div>
      <div className="Footer">
         {/* add footer */}
