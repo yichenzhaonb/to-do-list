bc7ea4e043db98fc714697e44886f03c6dd727a0
yichen.zhao <yichen.zhao@americaneagle.com>
2020-09-30 11:54:04 -0500

    create repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          To Do List
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="Content">
+       {/* add content */}
+      </div>
+     <div className="Footer">
+        {/* add footer */}
+     </div>
     </div>
   );
 }
