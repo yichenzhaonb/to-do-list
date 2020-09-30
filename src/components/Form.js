66bac4c1acbb4d1eb54ece389654b67fc4b30ca4
Yichen <8408937+yichenzhaonb@users.noreply.github.com>
2020-09-30 12:45:05 -0500

    add Form (#1)

---
+++ src/components/Form.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+function Form(pros){
+    const [name,setName] = useState('');
+    function handleSubmit(e){
+       e.preventDefault();
+       if(!name.trim()){
+           return;
+       }
+       pros.addTaks(name);
+       setName("");
+    }
+ 
+    function handleChange(e){
+        setName (e.target.value); 
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+          <h2 className="label-wrapper">
+            <label htmlFor="new-todo-input" className="label__lg">
+              Whats in your mind today?
+            </label>
+          </h2>
+
+          <input
+            type="text"
+            id="new-todo-input"
+            className="input input__lg"
+            name="text"
+            autoComplete="off"
+            value={name}
+            onChange={handleChange}
+          />
+          <button type="submit" className="btn btn__primary btn__lg">
+            Add
+          </button>
+        </form>
+      );
+
+}
+
+export default Form;
