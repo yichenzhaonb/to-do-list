5ce9e1fec7517ab67c1ccc94dac0659822e7cae0
yichen.zhao <yichen.zhao@americaneagle.com>
2020-10-05 22:53:23 -0500

    Updates

---
+++ precache-manifest.853bf63121a9dd21d3f1bc1e4c6580c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b17b72e8ea3b0154fbd832029b25c100",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "f1ae535d6ab2b4f22b2a",
+    "url": "/todo/static/css/main.c8b56a32.chunk.css"
+  },
+  {
+    "revision": "cb64ff8f13b94d7f2d96",
+    "url": "/todo/static/js/2.c9b08154.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.c9b08154.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1ae535d6ab2b4f22b2a",
+    "url": "/todo/static/js/main.cb949f5a.chunk.js"
+  },
+  {
+    "revision": "a4ede8c04729d69d6b3f",
+    "url": "/todo/static/js/runtime-main.9a33a07b.js"
+  }
+]);
